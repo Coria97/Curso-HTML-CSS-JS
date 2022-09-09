81836db0a03017d43957153cff10bbf2cf92b386
Coria97 <santiagocoria@live.com.ar>
2022-09-09 15:00:11 -0300

    Filmina 45 set de filminas 5

---
+++ Curso JS/practica.js
@@ -0,0 +1,21 @@
+//Teoria Funcion
+//En JS una funcion puede o no devolver datos
+
+function saludar(){
+    alert("hola"); //Nos muestra una alerta en nuestro navegador. No se suele usar en paginas reales, ya que no se integra visualmente.
+}
+
+//Teoria Constantes
+//Declarar una constante
+const cantDados = 2;
+
+//Teoria variable
+//Declarar una variable
+let nombre = "pepe";
+
+//Teoria Evento
+//Un evento es algo que ocurre en el sistema, originado por el usuario o otra parte del sistema.
+//La forma correcta de declarar un evento, al documento le pido el elemento con ID texto, y a su contenido se lo piso con un nuevo valor
+document.querySelector('#texto').innerHTML= "Esto es un texto generado por JavaScript"; 
+
+//Filmina 45 set de filminas 5
